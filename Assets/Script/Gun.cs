44e04b25063aee0038d42f74b1044fdef03c8536
ask1666 <geeky_assb@live.no>
2020-02-05 16:53:05 +0100

    lagde våpensystem (beam). Kan ikke detecte noe enda

---
+++ Assets/Script/Gun.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    private LineRenderer lr;
+    public float beamRange;
+    public static bool shoot = false;
+    public float gunCooldown = 1f;
+    private float gunCooldownTimer = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        lr = gameObject.GetComponent<LineRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        gunCooldownTimer += Time.deltaTime;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up);
+        
+        
+    }
+
+    IEnumerator doBeam() {
+
+        lr.enabled = true;
+        lr.SetPosition(0, new Vector2(transform.position.x + 0.52f, transform.position.y + 0.7f));
+        lr.SetPosition(1, new Vector2(transform.position.x + 0.52f, transform.position.y + beamRange));
+        yield return new WaitForSeconds(0.2f);
+        lr.enabled = false;
+        lr.SetPosition(1, transform.position);
+        
+
+    }
+
+    public void OnClick() {
+        Debug.Log(gunCooldownTimer);
+        if (gunCooldownTimer > gunCooldown) {
+            StartCoroutine(doBeam());
+            shoot = false;
+            gunCooldownTimer = 0;
+        }
+    }
+
+    
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float speed = 0.01f;
+    public bool moveLeft = false;
+    public bool moveRight = false;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        /**
+         * If you touch the screen on the left player will move to the left,
+         * if you touch the screen on the right the player will move to the right.
+         * speed = the rate of speed the player will move at.
+        */
+
+        if (moveLeft) {
+
+            this.gameObject.transform.Translate(Vector2.left * (Time.deltaTime * speed));
+
+        } else if (moveRight) {
+
+            this.gameObject.transform.Translate(Vector2.right * (Time.deltaTime * speed));
+
+        }
+
+    }
+
+
+
+    public void OnLeftBtnDown() {
+        moveLeft = true;
+        Debug.Log("Left down");
+    }
+
+    public void OnLeftBtnUp() {
+        moveLeft = false;
+        Debug.Log("Left up");
+    }
+
+    public void OnRightBtnDown() {
+        moveRight = true;
+        Debug.Log("Right down");
+    }
+
+    public void OnRightBtnUp() {
+        moveRight = false;
+        Debug.Log("Right up");
+    }
+}
