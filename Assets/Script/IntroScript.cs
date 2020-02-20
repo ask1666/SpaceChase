e79aa1d3c47b020e702f3a2f835accb5620893b5
ask1666 <geeky_assb@live.no>
2020-02-20 17:14:15 +0100

    Laget grafikk for coin

--- Assets/Script/IntroScript.cs
+++ Assets/Script/IntroScript.cs
@@ -11,19 +11,19 @@ public class IntroScript : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        
-            SpaceShip = GameObject.Find("SpaceShip").GetComponent<Animator>();
-            Player = GameObject.Find("Player").GetComponent<Animator>();
-            playerJetPack = GameObject.Find("JetPack").GetComponent<ParticleSystem>();
-            AsteroidSpawn asteroidSpawner = GameObject.Find("AsteroidSpawner").GetComponent<AsteroidSpawn>();
-            PlayerController playercontroller = GameObject.Find("Player").GetComponent<PlayerController>();
-            asteroidSpawner.dontSpawn = true;
-            playercontroller.blockPlayerControl = true;
-            
-            StartCoroutine(StartIntro());
-            asteroidSpawner.dontSpawn = false;
-            playercontroller.blockPlayerControl = false;
-        
+        Time.timeScale = 1;
+        SpaceShip = GameObject.Find("SpaceShip").GetComponent<Animator>();
+        Player = GameObject.Find("Player").GetComponent<Animator>();
+        playerJetPack = GameObject.Find("JetPack").GetComponent<ParticleSystem>();
+        AsteroidSpawn asteroidSpawner = GameObject.Find("AsteroidSpawner").GetComponent<AsteroidSpawn>();
+        PlayerController playercontroller = GameObject.Find("Player").GetComponent<PlayerController>();
+        asteroidSpawner.dontSpawn = true;
+        playercontroller.blockPlayerControl = true;
+
+        StartCoroutine(StartIntro());
+        asteroidSpawner.dontSpawn = false;
+        playercontroller.blockPlayerControl = false;
+
     }
 
     // Update is called once per frame
