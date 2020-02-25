5796cb21b7033eebd03fa91c1e3e7709155b8d12
ask1666 <geeky_assb@live.no>
2020-02-25 17:49:09 +0100

    Upgrades GUI added

---
+++ Assets/Script/PowerUpExplosion.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpExplosion : MonoBehaviour {
+
+    public ParticleSystem explosion;
+    public GameObject graphic;
+    public Collider2D theCollider;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start() {
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag.Equals("Player")) {
+            StartCoroutine(doExplosion());
+            if (this.gameObject.tag.Equals("Coin")) {
+                Coin();
+            } else if (this.gameObject.tag.Equals("Fuel")) {
+                Fuel();
+            } else if (this.gameObject.tag.Equals("Ammo")) {
+                Ammo();
+            }
+        }
+    }
+
+    IEnumerator doExplosion() {
+
+        Destroy(graphic);
+        GetComponent<Rigidbody2D>().freezeRotation = true;
+        animator.enabled = false;
+        theCollider.enabled = false;
+        explosion.Play();
+        yield return new WaitForSeconds(0.4f);
+        Destroy(this.gameObject);
+
+    }
+
+    void Ammo() {
+
+    }
+
+    void Fuel() {
+
+    }
+
+    void Coin() {
+        GameObject.Find("GameControl").GetComponent<Score>().cash += 1;
+    }
+}
