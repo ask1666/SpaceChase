d178d76f50b325938be14e2dfa78fd3dfbec36c3
ask1666 <geeky_assb@live.no>
2020-03-05 20:36:42 +0100

    music adjustments

---
+++ Assets/Script/AudioController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioController : MonoBehaviour {
+
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        SceneManager.activeSceneChanged += onSceneChanged;
+        
+    }
+
+    private void onSceneChanged(Scene previousScene, Scene nextScene) {
+        try {
+            AudioSource foundAudioSource = GameObject.Find("AudioSource").GetComponent<AudioSource>();
+            if (foundAudioSource != null) {
+                this.audioSource.mute = true;
+            }
+            Debug.Log(foundAudioSource);
+        } catch (NullReferenceException) {
+            if (this.audioSource.mute) {
+                this.audioSource.Play();
+                this.audioSource.mute = false;
+            }
+        }
+    }
+}
