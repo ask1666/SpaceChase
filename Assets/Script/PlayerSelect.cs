bff1ceb5182353f85cf4055874c4a82a492dfc4c
ask1666 <geeky_assb@live.no>
2020-03-26 18:09:03 +0100

    player unlock system

--- Assets/Script/PlayerSelect.cs
+++ Assets/Script/PlayerSelect.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerSelect : MonoBehaviour {
 
     private UpgradesProperties UP;
     private Score score;
+    public Button player2, player3, player4, player;
 
     public void Start() {
         UP = GameObject.Find("GameControl").GetComponent<UpgradesProperties>();
         score = GameObject.Find("GameControl").GetComponent<Score>();
+
+        if (score.highScore >= 200) {
+            player2.interactable = true;
+            player3.interactable = true;
+            player3.gameObject.GetComponentInChildren<Text>().gameObject.SetActive(false);
+            player4.interactable = false;
+            player.interactable = false;
+        } if (score.highScore >= 500) {
+            player2.interactable = true;
+            player3.interactable = true;
+            player4.interactable = true;
+            player4.gameObject.GetComponentInChildren<Text>().gameObject.SetActive(false);
+            player.interactable = false;
+        } if (score.highScore >= 1000) {
+            player2.interactable = true;
+            player3.interactable = true;
+            player4.interactable = true;
+            player.interactable = true;
+            player.gameObject.GetComponentInChildren<Text>().gameObject.SetActive(false);
+        } if (score.highScore < 200) {
+            player2.interactable = true;
+            player3.interactable = false;
+            player4.interactable = false;
+            player.interactable = false;
+        }
     }
 
     public void OnPlayerClick() {
