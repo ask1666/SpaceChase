e81badb12c9fa13c94b02bdf69803641f6abadd4
ask1666 <geeky_assb@live.no>
2020-04-10 03:50:25 +0200

    bug fixes

--- Assets/Script/ObstacleExplosion.cs
+++ Assets/Script/ObstacleExplosion.cs
@@ -79,6 +79,6 @@ public class ObstacleExplosion : MonoBehaviour {
     void Garbage() {
         
         GameObject player = GameObject.FindGameObjectWithTag("Player");
-        player.transform.Translate(Vector2.down * 1f);
+        player.transform.Translate(Vector2.down * 2.5f);
     }
 }
